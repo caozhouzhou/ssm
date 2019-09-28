ed63a48cabeae8fde57c621c62c3e0fe9b3030dc
caozhou_zch <caozhou_zch@163.com>
2019-09-28 17:09:16 +0800

    ssm整合之编写SpringMVC框架

--- src/main/java/cn/jdwa/controller/AccountController.java
+++ src/main/java/cn/jdwa/controller/AccountController.java
@@ -1,11 +1,18 @@
 package cn.jdwa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by czz on 2019/9/28.
  */
 @Controller
+@RequestMapping("/account")
 public class AccountController {
 
+    @RequestMapping("/findAll")
+    public String findAll(){
+        System.out.println("表现层:查询所有账户信息。。。 。。。");
+        return "list";
+    }
 }
--- src/main/webapp/WEB-INF/pages/list.jsp
+++ src/main/webapp/WEB-INF/pages/list.jsp
@@ -2,15 +2,15 @@
   Created by IntelliJ IDEA.
   User: czz
   Date: 2019/9/28
-  Time: 16:45
+  Time: 16:49
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>测试</title>
+    <title>账户列表</title>
 </head>
 <body>
-    <a href="account/findAll">查询账户列表</a>
+    <h3>账户列表</h3>
 </body>
 </html>
