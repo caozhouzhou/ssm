3fc0c06d8bdf9a391fc81adf129584f727ae9c6c
caozhou_zch <caozhou_zch@163.com>
2019-09-30 23:43:49 +0800

    ssm整合之Spring整合Mybatis

--- src/main/java/cn/jdwa/service/impl/AccountServiceImpl.java
+++ src/main/java/cn/jdwa/service/impl/AccountServiceImpl.java
@@ -1,7 +1,9 @@
 package cn.jdwa.service.impl;
 
+import cn.jdwa.dao.AccountDao;
 import cn.jdwa.domain.Account;
 import cn.jdwa.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,14 +13,22 @@ import java.util.List;
  */
 @Service("accountService")
 public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao dao;
     // 查询所有账户
     public List<Account> findAll(){
         System.out.println("业务层：查询所有账户。。。");
-        return null;
+        List<Account> accounts = dao.findAll();
+        for (Account ac:accounts){
+            System.out.println("dao查询结果 --》"+ac);
+        }
+        return dao.findAll();
     }
 
     // 保存帐户信息
     public void saveAccount(Account account){
         System.out.println("业务层：保存账户。。。");
+        dao.saveAccount(account);
     }
 }
--- src/main/java/cn/jdwa/test/TestSpring.java
+++ src/main/java/cn/jdwa/test/TestSpring.java
@@ -1,5 +1,6 @@
 package cn.jdwa.test;
 
+import cn.jdwa.domain.Account;
 import cn.jdwa.service.AccountService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -18,5 +19,9 @@ public class TestSpring {
         AccountService as = (AccountService) ac.getBean("accountService");
         // 调用方法
         as.findAll();
+        Account acc = new Account();
+        acc.setName("admin");
+        acc.setMoney(12.0);
+        as.saveAccount(acc);
     }
 }
